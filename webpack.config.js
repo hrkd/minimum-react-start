639380a11e3725f07a6cd95f25fbfced69d2b7c0
hrkd <hrkd@users.noreply.github.com>
2019-03-28 19:45:25 +0900

    typescript (#2)
    * init
    
    * install typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
-require('babel-polyfill');
 var webpack = require('webpack');
 var commonConfig = require('./webpack.common.config');
 
 module.exports = Object.assign(commonConfig, {
   mode: 'development',
-  entry: ['babel-polyfill', 'webpack-hot-middleware/client', './source/javascripts/index'],
+  entry: ['webpack-hot-middleware/client', './source/javascripts/index.tsx'],
   plugins: [new webpack.HotModuleReplacementPlugin()],
   devtool: '#eval'
 });
